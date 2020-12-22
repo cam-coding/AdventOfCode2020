703f1f2c49dc8cea0a3140dccd6f0ce28eea862c
Cam Thompson <camstercammy@hotmail.com>
2020-12-22 12:32:21 -0500

    Day 22: Fixed and cleaned

--- 22/first.py
+++ 22/first.py
@@ -1,6 +1,3 @@
-from collections import deque
-import math
-import pprint
 import copy
 
 def get_lines():
@@ -8,54 +5,31 @@ def get_lines():
         return [line.strip() for line in f.readlines() if line.strip()]
 
 Lines = get_lines()
-Width = len(Lines[0])
-Height = len(Lines)
 
-global p1Deck
-p1Deck = []
-global p2Deck
-p2Deck = []
+global decks
+decks = [ [], [] ]
 
 def get_decks():
     i = 1
     while Lines[i] != 'Player 2:' and i < len(Lines):
-        p1Deck.append(int(Lines[i]))
+        decks[0].append(int(Lines[i]))
         i += 1
     
     i +=1
-    while i < Height:
-        p2Deck.append(int(Lines[i]))
+    while i < len(Lines):
+        decks[1].append(int(Lines[i]))
         i += 1
 
 def play_war():
-    while len(p1Deck) > 0 and len(p2Deck) > 0:
-        card1 = p1Deck.pop(0)
-        card2 = p2Deck.pop(0)
-
-        if card1 > card2:
-            p1Deck.append(card1)
-            p1Deck.append(card2)
-        else:
-            p2Deck.append(card2)
-            p2Deck.append(card1)
-    print(p1Deck)
-    print("hi")
-    print(p2Deck)
+    while len(decks[0]) > 0 and len(decks[1]) > 0:
+        cards = [ decks[0].pop(0), decks[1].pop(0) ]
+        winner = int(cards[1] > cards[0])
+        decks[winner].append(cards[winner])
+        decks[winner].append(cards[1 - winner])
 
 def calc_score(deck):
-    total = 0
-    multiplier = 1
-    for i in range(len(deck)-1, -1, -1):
-        total += deck[i] * multiplier
-        #print(deck[i] * multiplier)
-        multiplier += 1
-    print(total)
+    print(sum([count * card for count, card in enumerate(reversed(deck), 1)]))
 
 get_decks()
-print(p1Deck)
-print(p2Deck)
 play_war()
-if len(p1Deck) == 0:
-    calc_score(p2Deck)
-else:
-    calc_score(p1Deck)
+calc_score(decks[int(len(decks[0]) == 0)])
--- 22/second.py
+++ 22/second.py
@@ -20,14 +20,18 @@ def get_decks():
     return decks
 
 def play_war_recursion(p1, p2, top):
+    # Reddit showed me the speed
+    if not top:
+        if max(p1) > max(p2):
+            return 0
     roundStorage = []
     cards = [0, 0]
     decks = [p1, p2]
     while len(decks[0]) > 0 and len(decks[1]) > 0:
         for round in roundStorage:
             if decks[0] == round[0] and decks[1] == round[1]:
-                return 1
-        roundStorage.append([copy.deepcopy(decks[0]), copy.deepcopy(decks[1])])
+                return 0
+        roundStorage.append(copy.deepcopy(decks))
         for i in range(2): cards[i] = decks[i].pop(0)
 
         winner = 0
@@ -42,13 +46,9 @@ def play_war_recursion(p1, p2, top):
     return int(len(decks[0]) == 0)
 
 def calc_score(deck):
-    print(deck)
-    total = 0
-    multiplier = 1
-    for i in range(len(deck)-1, -1, -1):
-        total += deck[i] * multiplier
-        multiplier += 1
-    print(total)
+    print(len(deck))
+    print(deck[0])
+    print(sum([count * card for count, card in enumerate(reversed(deck), 1)]))
 
 decks = get_decks()
 calc_score(play_war_recursion(copy.deepcopy(decks[0]), copy.deepcopy(decks[1]), True))
